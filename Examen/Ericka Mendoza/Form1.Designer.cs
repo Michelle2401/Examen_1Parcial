ff09e56bfbf7f7dabff97c3e5d081d3e86d151ce
Michelle2401 <116472057+Michelle2401@users.noreply.github.com>
2023-02-23 21:31:52 -0600

    creando menu para formularios

---
+++ Examen/Ericka Mendoza/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Ericka_Mendoza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.formulario1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formulario2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formulario3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formulario1ToolStripMenuItem,
+            this.formulario2ToolStripMenuItem,
+            this.formulario3ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // formulario1ToolStripMenuItem
+            // 
+            this.formulario1ToolStripMenuItem.Name = "formulario1ToolStripMenuItem";
+            this.formulario1ToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.formulario1ToolStripMenuItem.Text = "Formulario 1";
+            this.formulario1ToolStripMenuItem.Click += new System.EventHandler(this.formulario1ToolStripMenuItem_Click);
+            // 
+            // formulario2ToolStripMenuItem
+            // 
+            this.formulario2ToolStripMenuItem.Name = "formulario2ToolStripMenuItem";
+            this.formulario2ToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.formulario2ToolStripMenuItem.Text = "Formulario 2";
+            // 
+            // formulario3ToolStripMenuItem
+            // 
+            this.formulario3ToolStripMenuItem.Name = "formulario3ToolStripMenuItem";
+            this.formulario3ToolStripMenuItem.Size = new System.Drawing.Size(86, 20);
+            this.formulario3ToolStripMenuItem.Text = "Formulario 3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem formulario1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formulario2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formulario3ToolStripMenuItem;
+    }
+}
+
